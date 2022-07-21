cd68cbb1dde676628b35f30a0835e2e3cc4ca59a
Mini256 <minianter@foxmail.com>
2022-07-21 07:38:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/api/posts.js
@@ -0,0 +1,36 @@
+import prisma from '../../lib/prisma'
+
+export default async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const users = await prisma.post.findMany({
+        include: { author: true },
+      })
+      res.status(200).json(users)
+    } catch (error) {
+      console.error(error)
+      res.status(500).json(error)
+    }
+  } else if (req.method === 'POST') {
+    const { title, content, authorEmail } = req.body
+    try {
+      const createdPost = await prisma.post.create({
+        data: {
+          title,
+          content,
+          author: {
+            connect: {
+              email: authorEmail,
+            },
+          },
+        },
+      })
+      res.status(200).json(createdPost)
+    } catch (e) {
+      console.error(e)
+      return res.status(500)
+    }
+  } else {
+    res.status(404)
+  }
+}
