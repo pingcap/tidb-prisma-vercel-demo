adeba4a9eed813aae9b641e78ed183a3d8c7820d
Mini256 <minianter@foxmail.com>
2022-07-27 08:44:02 +0800

    refactor: use TiDB instead of PGSQL

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Vercel deployment example
+# TiDB example with Prisma and Vercel
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fprisma%2Fdeployment-example-vercel&env=DATABASE_URL&envDescription=PostgreSQL%20connection%20string&envLink=https%3A%2F%2Fwww.prisma.io%2Fdocs%2Fconcepts%2Fdatabase-connectors%2Fpostgresql%23connection-url&project-name=prisma-vercel-deployment-example&repo-name=prisma-vercel-deployment-example)
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FMini256%2Ftidb-prisma-vercel-demo&repository-name=tidb-prisma-demo&env=DATABASE_URL&envDescription=TiDB%20Cloud%20connection%20string&envLink=envLink=https%3A%2F%2Fdocs.pingcap.com%2Ftidb%2Fdev%2Fdev-guide-build-cluster-in-cloud&project-name=tidb-prisma-demo)
 
 [Deployment guide](https://www.prisma.io/docs/guides/deployment/deploying-to-vercel)
--- prisma/migrations/20211004163457_init/migration.sql
+++ prisma/migrations/20211004163457_init/migration.sql
@@ -1,36 +1,24 @@
 -- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "content" TEXT,
-    "title" TEXT NOT NULL,
-    "authorId" INTEGER,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+CREATE TABLE `Post` (
+    `id` BIGINT AUTO_RANDOM PRIMARY KEY,
+    `content` TEXT,
+    `title` TEXT NOT NULL,
+    `authorId` BIGINT
 );
 
 -- CreateTable
-CREATE TABLE "Profile" (
-    "id" SERIAL NOT NULL,
-    "bio" TEXT,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+CREATE TABLE `Profile` (
+    `id` BIGINT AUTO_RANDOM PRIMARY KEY,
+    `bio` TEXT,
+    `userId` BIGINT NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+CREATE TABLE `User` (
+    `id` BIGINT AUTO_RANDOM PRIMARY KEY,
+    `email` VARCHAR(150) NOT NULL,
+    `name` TEXT
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+CREATE UNIQUE INDEX `User_email_key` ON `User`(`email`);
