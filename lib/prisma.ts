1af34b5d296cb4868af00d92b58a0aa316ca7783
czhen <56986964+shczhen@users.noreply.github.com>
2022-09-14 10:56:36 +0800

    Revert "add sslcert support"
    This reverts commit 50d8f3c7bcc17d54e1d07e7e4ae5b2ed244a1da0.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { writeFileSync } from "fs";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -26,21 +25,11 @@ const DATABASE_URL =
     ? `mysql://${process.env.TIDB_USER}:${process.env.TIDB_PASSWORD}@${process.env.TIDB_HOST}:${process.env.TIDB_PORT}/bookshop`
     : process.env.DATABASE_URL;
 
-const generateSSLCA = (content?: string) => {
-  if (!content) {
-    // console.log("No SSL CA provided");
-    return "";
-  }
-  // console.log("SSL CA provided");
-  writeFileSync("./ssl-ca.cert", content);
-  return `?sslcert=../ssl-ca.cert`;
-};
-
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient({
     datasources: {
       db: {
-        url: DATABASE_URL + generateSSLCA(process.env.TIDB_SSL_CA),
+        url: DATABASE_URL,
       },
     },
   });
@@ -49,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
     global.prisma = new PrismaClient({
       datasources: {
         db: {
-          url: DATABASE_URL + generateSSLCA(process.env.TIDB_SSL_CA),
+          url: DATABASE_URL,
         },
       },
     });
